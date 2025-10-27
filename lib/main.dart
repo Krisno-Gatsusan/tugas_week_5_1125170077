c52547926407beb0f0afbadb7881d0e887f74c3b
Krisno-Gatsusan <kgatsusan@gmail.com>
2025-10-27 11:25:22 +0700

    pembuatan title scafold

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,22 @@ class KR extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Tugas 5 1125170077",
+      home: MyKR(),
+    );
+  }
+}
+
+class MyKR extends StatelessWidget {
+  const MyKR({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TUGAS 5"),
+        backgroundColor: Colors.orange,
+        actions: [Icon(Icons.person, color: Colors.white)],
+      ),
     );
   }
 }
